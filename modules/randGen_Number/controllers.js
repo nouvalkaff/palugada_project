9782d6aa56e1e8ce52ce71c791d56955d6bc3d50
M Nouval A Alkaff <abdelnouval@gmail.com>
2023-03-21 01:22:19 +0700

    optimize the number generator capability

--- modules/randGen_Number/controllers.js
+++ modules/randGen_Number/controllers.js
@@ -2,17 +2,7 @@ const { generateNumber } = require('./helper_function');
 
 exports.genNumber = (req, res) => {
   try {
-    // setTimeout(() => {
-    //   return res.status(408).send({
-    //     code: 408,
-    //     codeMessage: 'Request Timeout',
-    //     success: false,
-    //     message: `The request time exceeds 2 seconds, please try again.`
-    //   });
-    // }, 2000);
-
     let format = req.query;
-
     const { maxnum, length } = format;
 
     if (maxnum > 10000 || length > 2000) {
