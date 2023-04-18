45f2fcc516cb370f1090a5f1fe391f764055e338
M Nouval A Alkaff <mohamad.nouval@dansmultipro.com>
2023-04-18 18:10:38 +0700

    save progress

--- modules/usernameGenerator/helper_function.js
+++ modules/usernameGenerator/helper_function.js
@@ -28,9 +28,12 @@ const randomUsernameGenerator = async (preset) => {
 
   const getContent = JSON.parse(fs.readFileSync(path, { encoding: 'utf8' }));
 
-  getContent[type].push(randomWord);
+  const isExist = getContent[type].find((element) => element === randomWord);
 
-  fs.writeFileSync(path, JSON.stringify(getContent));
+  if (!isExist) {
+    getContent[type].push(randomWord);
+    fs.writeFileSync(path, JSON.stringify(getContent));
+  }
 
   if (!preset) {
     let isGenerating = [true, false];
