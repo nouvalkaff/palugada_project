09aadcd638ac6e92a5053ea1afc1fbe143b291c5
M Nouval A Alkaff <mohamad.nouval@dansmultipro.com>
2023-04-18 23:38:28 +0700

    Add DB postgres live

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -15,12 +15,11 @@ const DBConfig = {
     dialect: 'postgres'
   },
   production: {
-    username: 'xenanzef',
-    password: 'tzf6_eMh8gOwkOsmNhzZpx9kCNfSFyRu',
-    database: 'xenanzef',
-    host: 'rosie.db.elephantsql.com',
-    logging: false,
-    dialect: 'postgres'
+    username: process.env.DEV_USERNAME,
+    password: process.env.DEV_PASSWORD,
+    database: process.env.DEV_DATABASE,
+    host: process.env.DEV_HOST,
+    dialect: process.env.DEV_DIALECT
   }
 };
 
