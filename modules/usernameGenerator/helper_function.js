1d8c294c8460697659b2b033883cb6f6aae9ed21
M Nouval A Alkaff <mohamad.nouval@dansmultipro.com>
2023-04-18 15:35:13 +0700

    add logic to use animal name if preset is not exist

--- modules/usernameGenerator/helper_function.js
+++ modules/usernameGenerator/helper_function.js
@@ -1,5 +1,7 @@
 const { RAND_QUERY } = require('../../constant/usernameGen');
 const { generateRandomNumber } = require('../general_function_helper');
+const { animals } = require('../randGenAnimal/animals_name_en');
+
 const [fixedQuery, ...rest] = RAND_QUERY;
 
 const getQuery = () => {
@@ -13,13 +15,13 @@ const randomUsernameGenerator = async (preset) => {
 
   let URI = `${process.env.RAND_WORD_URI}?type=${type}`;
 
+  //Generate Username Below
   if (type === 'adverb') {
     let usingThe = ['', 'the'];
     usingThe = usingThe[generateRandomNumber(2, 0)];
     theUsername = placement.replace('!', usingThe);
   }
 
-  //generate random word below
   const response = await fetch(URI, {
     method: 'get',
     headers: { 'X-Api-Key': process.env.RAND_WORD_API_KEY }
@@ -29,20 +31,31 @@ const randomUsernameGenerator = async (preset) => {
   theUsername = theUsername.replace('$', randomWord);
 
   if (!preset) {
-    URI = `${process.env.RAND_WORD_URI}?type=${fixedQuery}`;
+    let isGenerating = [true, false];
+    isGenerating = isGenerating[generateRandomNumber(2, 0)];
 
-    const response = await fetch(URI, {
-      method: 'get',
-      headers: { 'X-Api-Key': process.env.RAND_WORD_API_KEY }
-    });
+    if (isGenerating) {
+      URI = `${process.env.RAND_WORD_URI}?type=${fixedQuery}`;
 
-    const { word: generatedPreset } = await response.json();
-    theUsername = theUsername.replace('?', generatedPreset);
-  } else {
-    theUsername = theUsername.replace('?', preset);
+      const response = await fetch(URI, {
+        method: 'get',
+        headers: { 'X-Api-Key': process.env.RAND_WORD_API_KEY }
+      });
+
+      const { word: generatedPreset } = await response.json();
+      theUsername = theUsername.replace('?', generatedPreset);
+      return theUsername.toLowerCase();
+    } else {
+      animalPreset = animals[generateRandomNumber(animals.length, 0)];
+      animalPreset = animalPreset.replace(' ', '');
+
+      theUsername = theUsername.replace('?', animalPreset);
+      return theUsername.toLowerCase();
+    }
   }
 
-  return theUsername;
+  theUsername = theUsername.replace('?', preset);
+  return theUsername.toLowerCase();
 };
 
 module.exports = { randomUsernameGenerator };
