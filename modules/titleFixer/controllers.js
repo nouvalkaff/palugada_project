042bfd4221adb46af8781ae366968727cdd95e82
M Nouval Abdel <mohamad.nouval@dansmultipro.com>
2023-07-20 12:16:47 +0700

    Update rate from 0.1 to 10000 Rupiah

--- modules/titleFixer/controllers.js
+++ modules/titleFixer/controllers.js
@@ -1,9 +1,16 @@
 const { titleFixerService } = require('./helper_function');
 
 exports.titleFixer = (req, res) => {
-  const { judul } = req.query;
+  const { judul, simple } = req.query;
 
   const fixedTitle = titleFixerService(judul);
+
+  if (simple === '1') {
+    return res
+      .setHeader('Content-type', 'text/html')
+      .status(200)
+      .send(` Selesai, mohon cek lagi ==> ${fixedTitle} <==`);
+  }
 
   return res.status(200).send({
     code: 200,
