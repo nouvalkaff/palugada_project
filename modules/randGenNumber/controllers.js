516feb59dd45e205203056670d8d004b1591c154
M Nouval Abdel <mohamad.nouval@dansmultipro.com>
2023-07-22 20:16:13 +0700

    Add handler for maximum number and length for simple generate number

--- modules/randGenNumber/controllers.js
+++ modules/randGenNumber/controllers.js
@@ -5,14 +5,7 @@ exports.genNumber = (req, res) => {
     let format = req.query;
     const { maxnum, length } = format;
 
-    if (+format.simple === 1) {
-      return res
-        .setHeader('Content-type', 'text/html')
-        .status(200)
-        .send(`==> ${generateNumber(format)} <==`);
-    }
-
-    if (maxnum > 10000 || length > 2000) {
+    if (+format.simple === 0 && (maxnum > 10000 || length > 2000)) {
       return res.status(400).send({
         code: 400,
         codeMessage: 'Bad Request',
@@ -20,6 +13,20 @@ exports.genNumber = (req, res) => {
         message:
           'The max amount of maximum number is 10,000 and length is 2,000'
       });
+    } else if (+format.simple === 1 && (maxnum > 100000 || length > 10)) {
+      return res.status(400).send({
+        code: 400,
+        codeMessage: 'Bad Request',
+        success: false,
+        message: 'The max amount of maximum number is 100,000 and length is 10'
+      });
+    }
+
+    if (+format.simple === 1) {
+      return res
+        .setHeader('Content-type', 'text/html')
+        .status(200)
+        .send(`==> ${generateNumber(format)} <==`);
     }
 
     const data = generateNumber(format);
